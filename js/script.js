e54328996f5a9a1f7429d7b5af2133706379f9ad
Shortrope <mario@shortrope.com>
2016-05-05 01:18:26 +0000

    Add script.js:
    Handles the form. Adds data to local db using PouchDB

--- js/script.js
+++ js/script.js
@@ -1,4 +1,64 @@
+// Wait for DOM creation before accessing elements 
 $(document).ready(function () {
-  var db = new PouchDB('scc');  
+    
+
+  // Create a reference to the local PouchDB
+  var db = new PouchDB('scc_mario');
+
+  db.info().then(function (info) {
+    log('db_name: ' + info.db_name);
+    log('doc_count: ' + info.doc_count);
+  });
   
+  // Get reference to all form elements
+  var $firstname = $('#firstname'),
+      $lastname = $('#lastname'),
+      $email = $('#email'),
+      $phone = $('#phone'),
+      $subject = $('#subject'),
+      $message = $('#message'),
+      $submitBtn = $('#submit-btn'),
+      $resetBtn = $('#reset-btn');
+
+
+  // Our submit event handler
+  $submitBtn.on('click', function (evt) {
+    evt.preventDefault();
+    
+    // Validate form fields
+    
+    // Prepare the JSON doc
+    var doc = {
+      "_id": $email.val(),
+      "firstname": $firstname.val(),
+      "lastname": $lastname.val(),
+      "phone": $phone.val(),
+      "subject": $subject.val(),
+      "message": $message.val()
+    }
+    
+    // Add the JSON data to the local PouchDB
+    db.put(doc);
+    
+    // Clear all form fields
+    $firstname.val('');
+    $lastname.val('');
+    $email.val('');
+    $phone.val('');
+    $subject.val('');
+    $message.val('');
+    $submitBtn.val('');
+    $resetBtn.val('');
+
+  });
+
+
+
+
+/////////////////////////////////////////////////////////////
+//  Helper Functions
+
+  function log(msg) {
+    window.console.log(msg);
+  }
 });
